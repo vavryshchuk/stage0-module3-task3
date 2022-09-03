ddbb189d4f4430e343c24d7e05063c14961c4e3d
vavryshchuk <112618316+vavryshchuk@users.noreply.github.com>
2022-09-03 14:20:36 +0300

    Update DeclaringVars.java

--- src/main/java/lang/print/gaps/task3/DeclaringVars.java
+++ src/main/java/lang/print/gaps/task3/DeclaringVars.java
@@ -1,7 +1,9 @@
 package lang.print.gaps.task3;
 
 public class DeclaringVars {
-    public static void main(String[] args) {
-        //write code here
-    }
+   public static void main(String[] args) {
+        int a, b, c;
+        a=b=c=10;
+        System.out.println(c);//write code here 
+   }
 }
